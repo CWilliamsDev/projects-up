a0a9f892c3bebd5097f9aee5b905ab42a8a4de6f
CWilliamsDev <cjwilliams.3d@gmail.com>
2017-01-31 12:16:22 -0400

    Create bloodeffect.js

---
+++ shyft/bloodeffect.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var gib = this;
+var blood : GameObject;
+var hasLanded = false;
+var worldSpace : Transform;
+
+function Awake () {
+	gib.GetComponent.<Rigidbody>().AddForce(transform.forward * 50);
+}
+
+function OnCollisionEnter (col:Collision) {
+	if(col.gameObject.tag == "Floor" && !hasLanded){
+		var spawnBlood : GameObject;
+		spawnBlood = Instantiate(blood, transform.position, worldSpace.transform.rotation);
+	}
+}
