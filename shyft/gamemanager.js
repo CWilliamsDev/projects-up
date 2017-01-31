6842dd018bd0b5e9041c45a4f41a80aa061b84ed
CWilliamsDev <cjwilliams.3d@gmail.com>
2017-01-31 12:15:35 -0400

    Create gamemanager.js

---
+++ shyft/gamemanager.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var player1 : GameObject;
+var player2 : GameObject;
+
+var spawn1 : Transform;
+var spawn2 : Transform;
+
+var playersAlive = false;
+
+function Start () {
+	Spawn();
+}
+
+function Update () {
+
+}
+
+function Spawn () {
+	if (!playersAlive) {
+		var p1 : GameObject;
+		var p2 : GameObject;
+		p1 = Instantiate(player1, spawn1.transform.position, spawn1.transform.rotation);
+		p2 = Instantiate(player2, spawn2.transform.position, spawn2.transform.rotation);
+		playersAlive = true;
+	}
+}
