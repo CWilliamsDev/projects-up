ed0fb4b870be765e947ccc32a31ebc00c9ca61fe
CWilliamsDev <cjwilliams.3d@gmail.com>
2017-01-31 12:42:41 -0400

    Create signalSystem.cs

---
+++ carrier/signalSystem.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SignalSystem : MonoBehaviour {
+
+	// Array of Possible Towers
+	public GameObject[] signalTowers;
+
+    public GameObject signal;
+	public GameObject newSignal;
+
+	// Success 
+	public GameObject successImage;
+	public GameObject complete;
+	//public GameObject transmission;
+
+	void Start () {
+		signalTowers[0].gameObject.tag = "Origin";
+		InitializeSignal();
+    }
+
+    void Update(){
+        if (Input.GetKeyDown("left") || Input.GetKeyDown("a")) {
+            RotateLeft45();
+        }
+
+        if (Input.GetKeyDown("right") || Input.GetKeyDown("d"))
+        {
+            RotateRight45();
+        }
+    }
+
+	void InitializeSignal() {
+		newSignal = (GameObject) Instantiate (signal, signalTowers[0].transform.position, signalTowers[0].transform.rotation);
+	}
+
+	public void RotateLeft45 () {
+		newSignal.transform.Rotate(new Vector3(0, -45, 0));
+	}
+
+	public void RotateLeft90 () {
+		newSignal.transform.Rotate(new Vector3(0, -90, 0));
+	}
+
+	public void RotateRight45 () {
+		newSignal.transform.Rotate(new Vector3(0, 45, 0));
+	}
+
+	public void RotateRight90 () {
+		newSignal.transform.Rotate(new Vector3(0, 90, 0));
+	}
+
+	public void Success () {
+		successImage.SetActive(true);
+        //transmission.SetActive(false);
+        Debug.Log("LOAD SCENE");
+        StartCoroutine("Wait");
+    }
+
+    IEnumerator Wait(){
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
